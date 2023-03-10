128443cac244abbccc67968e1b8e83822fb2ad5b
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2023-03-10 23:09:16 +0200

    finish drag and drop

--- js/script.js
+++ js/script.js
@@ -1,139 +1,285 @@
+// -- Buttons
 const btnNot = document.querySelector(".btn-not");
 const btnIn = document.querySelector(".btn-in");
 const btnComp = document.querySelector(".btn-comp");
+
+// -- Progress List
 const notStartedList = document.querySelector(".not-started");
 const inProgressList = document.querySelector(".in-progress");
 const completedList = document.querySelector(".completed");
 const lists = document.querySelectorAll(".list");
 
-// Functions
-let task = {
-  id: Date.now(),
-  text: "",
+// - Init
+let tasksOne = [];
+
+let tasksTwo = [];
+
+let tasksThree = [];
+
+// if()
+const getStorage = function () {
+  const retrive1 = JSON.parse(localStorage.getItem("listOne"));
+  const retrive2 = JSON.parse(localStorage.getItem("listTwo"));
+  const retrive3 = JSON.parse(localStorage.getItem("listThree"));
+
+  tasksOne = retrive1 ?? [];
+  tasksTwo = retrive2 ?? [];
+  tasksThree = retrive3 ?? [];
 };
 
-// Add a task
-const addTask = function (progress) {
-  progress.innerHTML += `
-  <div class="task" draggable="true">
-    <div class="current-task">${task.text}</div>
-    <div>
-      <i class="fa-solid fa-pen"></i>
-      <i class="fa-solid fa-trash-can"></i>
-    </div>
-  </div>
-  `;
+getStorage();
+
+// - Functions
+const createTask = function (progress) {
+  if (progress.classList.contains("not-started")) {
+    progress.innerHTML = `<div class="empty"></div>`;
+
+    tasksOne.forEach((task) => {
+      const HTML = `
+      <div class="task-flex" draggable="true" data-ID="${task.id}">
+        <div class="task">${task.text}</div>
+        <div class="icons">
+          <i class="fa-solid fa-pen"></i>
+          <i class="fa-solid fa-trash-can"></i>
+        </div>
+        <div class="empty"></div>
+      </div>
+      `;
+
+      progress.innerHTML += HTML;
+    });
+  }
+
+  if (progress.classList.contains("in-progress")) {
+    progress.innerHTML = `<div class="empty"></div>`;
+
+    tasksTwo.forEach((task) => {
+      const HTML = `
+      <div class="task-flex" draggable="true" data-ID="${task.id}">
+        <div class="task">${task.text}</div>
+        <div class="icons">
+          <i class="fa-solid fa-pen"></i>
+          <i class="fa-solid fa-trash-can"></i>
+        </div>
+        <div class="empty"></div>
+      </div>
+      `;
+
+      progress.innerHTML += HTML;
+    });
+  }
+
+  if (progress.classList.contains("completed")) {
+    progress.innerHTML = `<div class="empty"></div>`;
+
+    tasksThree.forEach((task) => {
+      const HTML = `
+      <div class="task-flex" draggable="true" data-ID="${task.id}">
+        <div class="task">${task.text}</div>
+        <div class="icons">
+          <i class="fa-solid fa-pen"></i>
+          <i class="fa-solid fa-trash-can"></i>
+        </div>
+        <div class="empty"></div>
+      </div>
+      `;
+
+      progress.innerHTML += HTML;
+    });
+  }
+};
+createTask(notStartedList);
+createTask(inProgressList);
+createTask(completedList);
+
+const removeTask = function (id, progress) {
+  tasksOne = tasksOne.filter((task) => task.id != id);
+  tasksTwo = tasksTwo.filter((task) => task.id != id);
+  tasksThree = tasksThree.filter((task) => task.id != id);
+
+  saveDate("listOne", tasksOne);
+  saveDate("listTwo", tasksTwo);
+  saveDate("listThree", tasksThree);
+
+  createTask(progress);
+
+  dragAndDrop();
 };
 
-// Drag & Drop
+const updateTask = function (id, text, progress) {
+  const taskEl = tasksOne.find((task) => task.id == id);
+  const taskEl2 = tasksTwo.find((task) => task.id == id);
+  const taskEl3 = tasksThree.find((task) => task.id == id);
+
+  if (progress.classList.contains("not-started")) {
+    taskEl.text = text;
+    saveDate("listOne", tasksOne);
+  }
+
+  if (progress.classList.contains("in-progress")) {
+    taskEl2.text = text;
+    saveDate("listTwo", tasksTwo);
+  }
+
+  if (progress.classList.contains("completed")) {
+    taskEl3.text = text;
+    saveDate("listThree", tasksThree);
+  }
+
+  createTask(progress);
+};
+
+const changePosition = function () {};
+
+// - UI Interface
+const AddTask = function (progress) {
+  let newTask = {
+    id: Date.now(),
+    text: "New Task",
+  };
+
+  if (progress.classList.contains("not-started")) {
+    tasksOne.push(newTask);
+
+    createTask(progress);
+
+    saveDate("listOne", tasksOne);
+  }
+
+  if (progress.classList.contains("in-progress")) {
+    tasksTwo.push(newTask);
+
+    createTask(progress);
+    saveDate("listTwo", tasksTwo);
+  }
+
+  if (progress.classList.contains("completed")) {
+    tasksThree.push(newTask);
+
+    createTask(progress);
+    saveDate("listThree", tasksThree);
+  }
+  dragAndDrop();
+};
+
 const dragAndDrop = function () {
   document.addEventListener("dragstart", (e) => {
-    if (e.target.classList.contains("task")) {
-      e.target.classList.add("draggable");
+    if (e.target.classList.contains("task-flex")) {
+      e.target.classList.add("drag");
+
+      if (e.target.getAttribute("data-ID") == null) return;
+
+      e.dataTransfer.setData("text/plain", e.target.getAttribute("data-ID"));
     }
   });
 
   document.addEventListener("dragend", (e) => {
-    if (e.target.classList.contains("task")) {
-      e.target.classList.remove("draggable");
-    }
-  });
-
-  document.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    if (e.target.classList.contains("task")) {
-      e.target.classList.add("drag");
-    }
-  });
-
-  document.addEventListener("dragleave", (e) => {
-    if (e.target.classList.contains("task")) {
+    if (e.target.classList.contains("task-flex")) {
       e.target.classList.remove("drag");
     }
   });
 
-  document.addEventListener("drop", (e) => {
-    if (e.target.classList.contains("task")) {
-      e.target.classList.remove("drag");
-    }
-  });
+  const emptyEls = document.querySelectorAll(".empty");
 
-  lists.forEach((list) => {
-    list.addEventListener("drop", (e) => {
-      // e.preventDefault();
-      const curTask = document.querySelector(".draggable");
+  emptyEls.forEach((emptyEl) => {
+    emptyEl.addEventListener("dragover", (e) => {
+      e.preventDefault();
 
-      const afterElement = getDrag(list, e.clientY);
+      emptyEl.classList.add("draggable");
+    });
 
-      if (!afterElement) {
-        list.appendChild(curTask);
-      } else {
-        list.insertBefore(curTask, afterElement);
+    emptyEl.addEventListener("dragleave", (e) => {
+      emptyEl.classList.remove("draggable");
+    });
+
+    emptyEl.addEventListener("drop", (e) => {
+      emptyEl.classList.remove("draggable");
+
+      const contTasks = emptyEl.closest(".cont-tasks");
+      const emptyArr = [...contTasks.querySelectorAll(".empty")];
+      const emptyIndex = emptyArr.indexOf(emptyEl);
+
+      const taskID = e.dataTransfer.getData("text/plain");
+      const taskEl = document.querySelector(`[data-ID="${taskID}"]`);
+      const afterEl = emptyEl.parentElement.classList.contains("task-flex")
+        ? emptyEl.parentElement
+        : emptyEl;
+
+      const task = {
+        id: +taskID,
+        text: taskEl.innerText,
+      };
+
+      tasksOne = tasksOne.filter((task) => task.id != taskID);
+      tasksTwo = tasksTwo.filter((task) => task.id != taskID);
+      tasksThree = tasksThree.filter((task) => task.id != taskID);
+
+      if (contTasks.querySelector(".not-started")) {
+        tasksOne.splice(emptyIndex, 0, task);
       }
+
+      if (contTasks.querySelector(".in-progress")) {
+        tasksTwo.splice(emptyIndex, 0, task);
+      }
+
+      if (contTasks.querySelector(".completed")) {
+        tasksThree.splice(emptyIndex, 0, task);
+      }
+
+      saveDate("listOne", tasksOne);
+      saveDate("listTwo", tasksTwo);
+      saveDate("listThree", tasksThree);
+
+      afterEl.after(taskEl);
     });
   });
 };
+dragAndDrop();
 
-// To put element after element
-const getDrag = function (container, y) {
-  const els = container.querySelectorAll(".task:not(.draggable)");
-
-  let closestTask = null;
-  let closestOffset = Number.NEGATIVE_INFINITY;
-
-  els.forEach((task) => {
-    const { top } = task.getBoundingClientRect();
-
-    const offset = y - top;
-
-    if (offset < 0 && offset > closestOffset) {
-      closestOffset = offset;
-      closestTask = task;
-    }
-  });
-
-  return closestTask;
+// - Local Storage
+const saveDate = function (key, val) {
+  localStorage.setItem(key, JSON.stringify(val));
 };
 
-// Event listeners
+// - Event Listeners
 btnNot.addEventListener("click", () => {
-  dragAndDrop();
-  addTask(notStartedList);
+  AddTask(notStartedList);
 });
 
 btnIn.addEventListener("click", () => {
-  dragAndDrop();
-  addTask(inProgressList);
+  AddTask(inProgressList);
 });
 
 btnComp.addEventListener("click", () => {
-  dragAndDrop();
-  addTask(completedList);
+  AddTask(completedList);
 });
 
 window.addEventListener("click", (e) => {
   if (e.target.classList.contains("fa-trash-can")) {
-    e.target.parentElement.parentElement.remove();
+    const id = e.target.parentElement.parentElement.getAttribute("data-ID");
+    const progress = e.target.parentElement.parentElement.parentElement;
+    removeTask(id, progress);
   }
+});
 
+window.addEventListener("click", (e) => {
   if (e.target.classList.contains("fa-pen")) {
-    const curTask =
-      e.target.parentElement.parentElement.querySelector(".current-task");
-    curTask.setAttribute("contenteditable", "true");
-    curTask.focus();
+    const taskEl = e.target.parentElement.parentElement.querySelector(".task");
+    taskEl.setAttribute("contentEditable", "true");
+    taskEl.focus();
+
+    taskEl.addEventListener("blur", () => {
+      taskEl.removeAttribute("contentEditable");
+
+      const id = e.target.parentElement.parentElement.getAttribute("data-ID");
+
+      const text = taskEl.textContent;
+
+      const progress = e.target.parentElement.parentElement.parentElement;
+
+      updateTask(id, text, progress);
+    });
     dragAndDrop();
   }
-
-  if (
-    !e.target.classList.contains("fa-pen") &&
-    e.target.querySelector(".current-task") !== null
-  ) {
-    // e.target.querySelector(".current-task").removeAttribute("contenteditable");
-    // task.text = e.target.querySelector(".current-task").textContent;
-    e.target.querySelectorAll(".current-task").forEach((smTask) => {
-      smTask.removeAttribute("contenteditable");
-      task.text = smTask.textContent;
-      task.text = "";
-    });
-  }
+  dragAndDrop();
 });
